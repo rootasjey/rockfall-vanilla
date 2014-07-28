ce52d57bfd2cdb2b38ad82b961991ac15f532192
rootasjey <jeremiecorpinot@outlook.com>
2014-07-28 07:40:25 +0200

    Messages UI

--- public/js/squareui.js
+++ public/js/squareui.js
@@ -1,5 +1,5 @@
 
-// GLABAL VARS
+// GLOBALS VARS
 // -----------
 var _messageGlowID = null;
 
@@ -27,6 +27,8 @@ function PopulateSquareUI() {
 function HideSuareUIContent() {
     HideMiniIcons();
     HideSquareUIGame();
+    HideMessagePanel();
+    HideSecondPanel();
 }
 
 // Hide mini icons in square-ui
@@ -42,6 +44,8 @@ function HideMiniIcons() {
 }
 
 function HideSquareUIGame() {
+    if($(".square-ui-game").css("opacity") === "0") return;
+
     $(".square-ui-game").css({
 
     }).animate({
@@ -264,6 +268,8 @@ function ShowSecondPanel() {
 // Hide second-panel with style
 // ------------------------
 function HideSecondPanel() {
+    if($(".second-panel").css("opacity") === "0") return;
+
     $(".second-panel").css({
         top: '0px',
         opacity: '1',
@@ -295,26 +301,280 @@ function CreateMessagePanel() {
         opacity: "0",
     }).appendTo(".second-panel");
 
+
     // Add icons to the message-panel
+    // -------------
+    // Collapse icon
+    // ---------
     $("<img>", {
         src: "../icons/icon_plus.png",
         class: "mini-icon",
+        function: "new-message",
     }).appendTo(".second-panel-title");
+
+    // Inbox icon
+    $("<img>", {
+        src: "../icons/icon_filledbox.png",
+        class: "mini-icon",
+        function: "inbox",
+    }).appendTo(".second-panel");
+
+    $("<span>", {
+        class: "messages-counter",
+    }).appendTo(".second-panel");
+
+    MessagePanelEvents();
 }
 
+// Add events on .message-panel
+// ---------------------------
+function MessagePanelEvents() {
+    // Click event on new messge icon
+    // ------------------------------
+    $(".mini-icon[function='new-message']").click(function () {
+        if ($("form[class='new-message']").length) {
+            // If the new message form is displayed
+            // we hide it
+            CloseNewMessage();
+            ShowNewMessageIcon();
+        }
+        else {
+            // Show message's form
+            NewMessage();
+
+            // Show close icon
+            ShowCloseNewMessageIcon();
+        }
+    });
+
+    // Click event on inbox icon
+    // -------------------------
+    $(".mini-icon[function='inbox']").click(function () {
+
+        // add +1 to the counter
+        // ---------------------
+        if($(".messages-counter").html() !== '') {
+            var count = $(".messages-counter").html();
+            count++;
+            RefreshMessagesCounter(count);
+            CreateInboxMessages();
+            ShowInboxMessages();
+        }
+        else RefreshMessagesCounter('1');
+    });
+}
+
+
+// Update the messages counter
+// ------------------------------------
+function RefreshMessagesCounter(count) {
+    $(".messages-counter").css({
+        opacity: '0',
+    }).animate({
+        opacity: '1',
+    });
+    $(".messages-counter").html(count);
+}
+
+// Create the panel wich contains
+// individuals messages
+// -----------------------------
+function CreateInboxMessages() {
+    // Verify that this object hasn't been created yet
+    if($(".inbox-messages").length > 0) return;
+
+    // Messages Panel Container
+    $("<div>", {
+        class: "inbox-messages",
+    }).appendTo(".message-panel");
+
+    // Sub-section containing left arrow
+    $("<div>", {
+        class: "inbox-left-section",
+    }).appendTo(".inbox-messages");
+
+    // Sub-section containing messages
+    $("<div>", {
+        class: "inbox-center-section",
+    }).appendTo(".inbox-messages");
+
+    // Sub-section containing right arrow
+    $("<div>", {
+        class: "inbox-right-section",
+    }).appendTo(".inbox-messages");
+
+
+    // Add Left & Right Arrows
+    // to sub-panels
+    $("<img>", {
+        src: "../icons/previous-256.png",
+        class: "previous-content",
+    }).appendTo(".inbox-left-section");
+    $("<img>", {
+        src: "../icons/next-256.png",
+        class: "next-content",
+    }).appendTo(".inbox-right-section");
+
+
+    // Add individual message
+    for (var i = 0; i < 6; i++) {
+        var m = $("<div>", {
+            class: "message",
+        }).appendTo(".inbox-center-section");
+
+        $("<img>", {
+            class: "message-icon",
+            src: "../icons/icon_envelope.png",
+        }).appendTo(m);
+        m.append("<span> Informations </span>");
+    }
+}
+
+function ShowInboxMessages() {
+    var children = $(".inbox-center-section").children();
+
+    for (var i = 0; i < children.length; i++) {
+        children[i].css({
+            opaciy: '0',
+        }).animate({
+            opaciy: '0.5',
+        });
+    }
+}
+
+// Display close icon
+// -----------------------
+function ShowCloseNewMessageIcon() {
+    // Animate the icon with css
+    $(".mini-icon[function='new-message']")
+    .css('-webkit-transform', 'rotate(45deg)')
+    .css('-moz-transform', 'rotate(45deg)')
+    .css('-ms-transform', 'rotate(45deg)')
+    .css('-o-transform', 'rotate(45deg)')
+    .css('transform', 'rotate(45deg)');;
+}
+
+function ShowNewMessageIcon() {
+    // Animate the icon with css
+    $(".mini-icon[function='new-message']")
+    .css('-webkit-transform', 'rotate(0deg)')
+    .css('-moz-transform', 'rotate(0deg)')
+    .css('-ms-transform', 'rotate(0deg)')
+    .css('-o-transform', 'rotate(0deg)')
+    .css('transform', 'rotate(0deg)');;
+}
+
+// Show Message panel with style
+// --------------------------
 function ShowMessagePanel() {
+    // Show the .message-panel content
     $(".message-panel").appendTo(".second-panel").css({
         opacity: "0",
     }).animate({
         opacity: "1",
     });
+
+    // Show the inbox icon
+    // outside the .message-panel
+    // $(".mini-icon[function='inbox']").css({
+    // }).animate({
+    //     opacity: '0.2',
+    //     height: '30px',
+    //     width: '30px',
+    // });
 }
 
+// Hide Message panel with style
+// --------------------------
 function HideMessagePanel() {
+    if($(".message-panel").css("opacity") === "0") return;
+
+    // Hide the message panel
     $(".message-panel").appendTo(".second-panel").css({
         opacity: "1",
     }).animate({
         opacity: "0",
+    });
+
+    // Hide the inbox icon
+    // outside the message panel
+    // $(".mini-icon[function='inbox']").css({
+    // }).animate({
+    //     opacity: '0',
+    //     height: '0px',
+    //     width: '0px',
+    // });
+}
+
+// Create a message form
+// --------------------
+function NewMessage() {
+    // Create the form
+    var form = document.createElement('form');
+    form.className = "new-message";
+    form.style.opacity = '0';
+
+    // Add the form to the page
+    $(".message-panel").append(form);
+
+    // Form's title
+    $("<span>", {
+        class: "form-title",
+        html: "New message",
+    }).appendTo(".new-message");
+
+    // To
+    $("<input>", {
+        class: "new-message-to",
+        placeholder : "send to..."
+    }).appendTo(".new-message");
+
+    // Body
+    $("<textarea>", {
+        class: "new-message-body",
+        placeholder : "message..."
+    }).appendTo(".new-message");
+
+    // Submit Button
+    $("<input>", {
+        type: "submit",
+        value: "send",
+    }).appendTo(".new-message");
+
+
+    // Animate Form
+    // show with style
+    // ---------------
+    $(".new-message").css({
+        opacity: '0',
+        marginTop: '-50px',
+    }).animate({
+        opacity: '1',
+        marginTop: '0',
+    });
+}
+
+
+// Remove the new message form
+// -------------------------
+function CloseNewMessage() {
+    // Animate Form
+    // hide with style
+    // ---------------
+    $(".new-message").css({
+        opacity: '1',
+        marginTop: '0',
+    }).animate({
+        opacity: '0',
+        marginTop: '-50px',
+    });
+
+    // Remove the form
+    // after the animation
+    // (so with delay)
+    // ----------------
+    Delay(function () {
+        $("form[class='new-message']").remove();
     });
 }
 
@@ -322,13 +582,13 @@ function HideMessagePanel() {
 // to notify of new messages
 // >use css attribute
 // --------------------------------
-function NewMessage() {
+function NewMessageAlert() {
     $(".mini-icon[function='messages']").attr('isGlowing', 'true');
 }
 
 // Set messages' icon to default
 // ---------------------------------
-function NoNewMessage() {
+function NoNewMessageAlert() {
     $(".mini-icon[function='messages']").attr('isGlowing', 'false');
 }
 
